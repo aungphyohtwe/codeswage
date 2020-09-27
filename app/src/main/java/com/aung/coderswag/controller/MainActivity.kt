367e13e2c67cde5413158e3c7ee3707f42ce801a
Aung Phyo Htwe <aungphyohtwe@Aungs-MacBook-Pro.local>
2020-09-27 22:15:04 +0900

    create category list view

--- app/src/main/java/com/aung/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/aung/coderswag/controller/MainActivity.kt
@@ -2,11 +2,22 @@ package com.aung.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.aung.coderswag.R
+import com.aung.coderswag.model.Category
+import com.aung.coderswag.service.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var adapter : ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        CategoryListView.adapter = adapter
     }
 }
